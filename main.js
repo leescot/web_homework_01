3c2c9ca830be5b2fca237603c6dd13e2d8d32885
leescot <44012453+leescot@users.noreply.github.com>
2023-03-17 18:35:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const userInput = document.getElementById("user-input");
 const submitBtn = document.getElementById("submit-btn");
-const apiResponse = document.getElementById("api-response");
+//const apiResponse = document.getElementById("api-response");
 
 submitBtn.addEventListener("click", () => {
   // 從 OpenAI API 中獲取回應，並顯示在 apiResponse 元素中
 });
+
+
+    const api-response = document.getElementById("api-response");
+
+      submitBtn.addEventListener("click", () => {
+        const text = input.value;
+        const reversed = text.split("").reverse().join("");
+        result.textContent = reversed;
