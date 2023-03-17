ceef8a7265bcec23dc30b7d73aa5953eabf0e3e1
leescot <44012453+leescot@users.noreply.github.com>
2023-03-17 18:15:37 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+const userInput = document.getElementById("user-input");
+const submitBtn = document.getElementById("submit-btn");
+const apiResponse = document.getElementById("api-response");
+
+submitBtn.addEventListener("click", () => {
+  // 從 OpenAI API 中獲取回應，並顯示在 apiResponse 元素中
+});
